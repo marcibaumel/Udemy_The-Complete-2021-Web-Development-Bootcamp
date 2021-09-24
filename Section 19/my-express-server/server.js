a54665e92790b73a516f6492147c49b857cb938b
Baumel Márton <marcibaumel@gmail.com>
2021-09-24 13:00:59 +0200

    230

--- Section 19/my-express-server/server.js
+++ Section 19/my-express-server/server.js
@@ -7,6 +7,18 @@ app.get("/", function(req, res){
     res.send("<h1>Hello World</h1>");
 })
 
+app.get("/contact", function(req, res){
+    res.send("Contact me: Marci");
+});
+
+app.get("/bio", function(req, res){
+    res.send("It's my site pls!");
+});
+
+app.get("/hobby", function(req, res){
+    res.send("It's my site of hobbys!");
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });

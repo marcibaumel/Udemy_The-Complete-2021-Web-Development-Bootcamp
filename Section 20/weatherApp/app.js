2b76413b997b9796a64af37e805662e60c2858ed
Baumel Márton <marcibaumel@gmail.com>
2021-12-13 17:08:16 +0100

    243

--- Section 20/weatherApp/app.js
+++ Section 20/weatherApp/app.js
@@ -3,10 +3,15 @@ const https = require("https")
 const app = express();
 
 app.get("/", function(req,res){
-    https.get("api.openweathermap.org/data/2.5/weather?q=Budapest&appid=567ebce00948424e603dc2cc17cf33d9&units=metric")
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=Budapest&appid=567ebce00948424e603dc2cc17cf33d9&units=metric";
+    
+    https.get(url, function(response){
+        console.log(response.statusCode);
+    })
 
     res.send("Server is running!")
 })
+
 
 app.listen(3000, function(){
     console.log("Server is runing on port 3000.")

7add6527298ee74c96388b33eeccc8594a7fbb79
Baumel Márton <marcibaumel@gmail.com>
2021-12-13 17:01:47 +0100

    weather api

--- Section 20/weatherApp/app.js
+++ Section 20/weatherApp/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
+const https = require("https")
 const app = express();
 
 app.get("/", function(req,res){
+    https.get("api.openweathermap.org/data/2.5/weather?q=Budapest&appid=567ebce00948424e603dc2cc17cf33d9&units=metric")
+
     res.send("Server is running!")
 })
 

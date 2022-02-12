e799cccc9e968d1e9f2ccdf35bcccbaf99ea3568
Baumel Márton <marcibaumel@gmail.com>
2022-02-12 22:20:10 +0100

    228

--- Section 19/my-express-server/server.js
+++ Section 19/my-express-server/server.js
@@ -1,6 +1,14 @@
 //jshint esversion:6
 
+const { response } = require("express");
 const express = require("express");
 const app = express();
 
-app.listen(3000);
+app.get("/", function(req, res){
+    res.send("<h1>Hello</h1>")
+});
+
+
+app.listen(3000, function(){
+    console.log("Server started on port 3000");
+});

4578e4a764c148ed79a76e44d8bc30ab482f1718
Baumel Márton <marcibaumel@gmail.com>
2022-02-13 10:46:02 +0100

    234

--- Section 19/calculator/server.js
+++ Section 19/calculator/server.js
@@ -1,10 +1,11 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 
 
 app.get("/", function(req, res){
-    res.send("Hello World")
+    //console.log(__dirname);
+    res.sendFile(__dirname+"/index.html")
 })
 
 app.listen(3000, function(){

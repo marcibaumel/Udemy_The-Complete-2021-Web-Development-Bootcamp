98641f5ed87500d60e038b13c486d2e892828e80
Baumel Márton <marcibaumel@gmail.com>
2022-02-13 12:40:18 +0100

    235

--- Section 19/calculator/server.js
+++ Section 19/calculator/server.js
@@ -1,11 +1,22 @@
 const express = require("express");
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get("/", function(req, res){
     //console.log(__dirname);
     res.sendFile(__dirname+"/index.html")
+})
+
+
+
+app.post("/index.html", function(req, res){
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var sum = num1+num2
+    res.send("Result: "+ sum);
 })
 
 app.listen(3000, function(){
